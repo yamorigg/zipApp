71378486226e58fb628f6cf6342e9371e5daff59
yamori <near@ymail.ne.jp>
2023-08-14 12:05:21 +0900

    Created getAddress and getHistory function

--- public/main.js
+++ public/main.js
@@ -1,3 +1,44 @@
-function getAddress(){
+function getAddress(event) {
+    event.preventDefault();
+    document.getElementById('errorMsg').innerHTML = '';
+    document.getElementById('result').innerHTML = '';
+    console.log('getAddress')
+    const zipCode = document.getElementById('zipCode').value;
     
+    fetch('/api/getAddress.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'zipCode=' + encodeURIComponent(zipCode)
+    })
+    .then(response => response.text())
+    .then(text => {
+        if (text.startsWith('Error:')) {
+            document.getElementById('errorMsg').innerHTML = text;
+        } else {
+            document.getElementById('result').innerHTML = text;
+            }
+        })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+
+function getHistory(){
+    document.getElementById('history').innerHTML = '';
+    fetch('/api/getHistory.php')
+    .then(response => response.json())
+    .then(history => {
+        let historyDiv = document.getElementById('history');
+        history.forEach(item => {
+            let p = document.createElement('p');
+            p.textContent = item.zipCode + ' : ' + item.address;
+            historyDiv.appendChild(p);
+            });       
+    })
+    .catch(error => {
+        console.error(error);
+    });
 }
