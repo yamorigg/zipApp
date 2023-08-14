cbaa140c3763fff3c89db6ef06871b72bde5a5a3
yamori <near@ymail.ne.jp>
2023-08-14 12:40:12 +0900

    Creating save and getHistory

--- api/db.php
+++ api/db.php
@@ -6,4 +6,20 @@ class Address {
     public function __construct($dbConnection) {
         $this->db = $dbConnection;
     }
-    
+
+    public function save($zipCode, $address) {
+        $stmt = $this->db->prepare("INSERT INTO search_history (zip_Code, address) VALUES (:zipCode, :address)");
+        $stmt->bindParam(':zipCode', $zipCode);
+        $stmt->bindParam(':address', $address);
+        $stmt->execute();
+    }
+
+    public function getHistory() {
+        // 履歴を取得するメソッドを作成。件数は10件まで
+        $stmt = $this->db->prepare("SELECT * FROM search_history ORDER BY id DESC LIMIT 10");
+        $stmt->execute();
+        //fetchAllメソッドを使って、取得したデータを配列に変換している
+        $history = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return json_encode($history);
+    }
+}
